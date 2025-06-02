4c113a52157c5ea0fe111e48b6e57d48bc4325e8
vinixtz <160758185+vinixtz@users.noreply.github.com>
2025-06-02 12:07:17 -0300

    sss

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://wqrvadycionozczmqgoj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcnZhZHljaW9ub3pjem1xZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0OTUzOTIsImV4cCI6MjA2MjA3MTM5Mn0.mMVAn7kJQH8bLhijfug2E0lMvpjRkKRE7CC29IFNNt4'
+  'https://wueqgnysdtvdtgvjtlyy.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1ZXFnbnlzZHR2ZHRndmp0bHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU2NjYsImV4cCI6MjA2MjExMTY2Nn0.iwASgVlh3EMi_5z0nC8CUxj4AYcakk6_nYbwFZcdTN8'
 )
